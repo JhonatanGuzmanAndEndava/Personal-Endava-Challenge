e2ad95829419116975ac63a0aff0caf93c0f2a0a
Javier Troconis <Javier.Tronconis@endava.com>
2018-03-26 15:16:19 -0500

    Initial commit

---
+++ readers-nest-backend/src/main/java/com/endava/interns/readersnestbackend/ReadersNestBackendApplication.java
@@ -0,0 +1,12 @@
+package com.endava.interns.readersnestbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadersNestBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadersNestBackendApplication.class, args);
+	}
+}
