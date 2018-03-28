96509435dcf99fba73e4cf7e6d08bd0720134135
Jhonatan Guzman <jguzman@endava.com>
2018-03-28 15:06:43 -0500

    Adding basic template for bookclub

---
+++ readers-nest-backend/bookclub/src/main/java/com/endava/interns/readersnestbackedbookclub/ReadersNestBackedBookclubApplication.java
@@ -0,0 +1,12 @@
+package com.endava.interns.readersnestbackedbookclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadersNestBackedBookclubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadersNestBackedBookclubApplication.class, args);
+	}
+}
