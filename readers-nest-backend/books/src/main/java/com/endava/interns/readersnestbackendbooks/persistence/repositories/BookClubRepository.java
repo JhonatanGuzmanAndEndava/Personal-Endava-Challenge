f42100189365305a93004e5fe89fc408fd3eab3a
Javier Troconis <Javier.Tronconis@endava.com>
2018-04-11 17:13:49 -0500

    Fixed problem with book microservice

--- readers-nest-backend/books/src/main/java/com/endava/interns/readersnestbackendbooks/persistence/repositories/BookClubRepository.java
+++
@@ -1,8 +0,0 @@
-package com.endava.interns.readersnestbackendbooks.persistence.repositories;
-
-import com.endava.interns.readersnestbackendbooks.persistence.entities.BookClub;
-import org.springframework.data.repository.CrudRepository;
-
-public interface BookClubRepository extends CrudRepository<BookClub, Long>{
-
-}
