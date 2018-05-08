cc6f171bc4ef315d29e725449c4bb0a1b1eb008d
jguzman <Jhonatan.Guzman@endava.com>
2018-05-08 13:28:52 -0500

    Working with default network

--- readers-nest-graphql/src/users/models.js
+++ readers-nest-graphql/src/users/models.js
@@ -1,7 +1,7 @@
 import RestConnector from '../connector/restConnector';
 import { parseResponseFromPromise } from '../connector/responseParser';
 
-const USER_SERVICE_ROOT = 'http://172.21.0.6:9000/users/';
+const USER_SERVICE_ROOT = 'http://users:9000/users/';
 
 export default class Users {
   constructor({ header }) {
